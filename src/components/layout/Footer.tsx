4dd4d7d39866788c1957fc6da7be0a6e63966e2f
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-11-13 16:37:00 +0330

    added footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,12 +1,35 @@
 import Container from "@/components/container/Container"
+import Link from "next/link"
 
 
 const Footer = () => {
   return (
     <footer>
        <Container>
-        <div className="bg-slate-800 p-24 my-8 rounded">
-
+        <div className="bg-slate-800 p-24 my-8 rounded flex flex-wrap">
+          <div className="w-6/12">
+            <ul className="grid gap-8 text-white list-disc">
+              <li className="list-disc">
+                <Link href={"/"}>
+                خانه
+                </Link>
+              </li>
+              <li className="list-disc">
+                <Link href={"/store"}>
+                محصولات
+                </Link>
+              </li>
+              <li className="list-disc" >
+                <Link href={"/about"}>
+                درباره ما
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="w-6/12 text-white">
+           <p>به فروشگاه ما خوش ا مدید تمام محصولات ما دارای  گارانتی معتبر است</p>
+           <p>ادرس : تهران  میدان آزادی کوچه اول سمت راست</p>
+          </div>
         </div>
        </Container>
     </footer>
